6eeb76e50e01d02b493b91dd31c8cfe7eae35d63
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-03 17:35:41 +0800

    Update bh-mj-schedule-list component.

---
+++ component.jsx
@@ -0,0 +1,14 @@
+
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ScheduleList from "./schedule-list.js";
+
+ReactDOM.render( 
+	<ScheduleList
+		label="This is a schedule list">
+	</ScheduleList>,
+	document.getElementById( "root" ) 
+);
--- test.jsx
+++
@@ -1,9 +0,0 @@
-
-"use strict";
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import ScheduleList from "./schedule-list.js";
-
-ReactDOM.render( <ScheduleList></ScheduleList> );
